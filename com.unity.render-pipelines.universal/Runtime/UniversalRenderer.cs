f3fb20624c99697a92a6c69e7f0337beeb165d4b
Sandy Carter <sandy.carter@unity3d.com>
2021-01-08 15:05:39 -0500

    ColorGradingLutPass: Use int id

--- com.unity.render-pipelines.universal/Runtime/UniversalRenderer.cs
+++ com.unity.render-pipelines.universal/Runtime/UniversalRenderer.cs
@@ -467,7 +467,7 @@ namespace UnityEngine.Rendering.Universal
 
             if (generateColorGradingLUT)
             {
-                colorGradingLutPass.Setup(colorGradingLut);
+                colorGradingLutPass.Setup(colorGradingLut.id);
                 EnqueuePass(colorGradingLutPass);
             }
 
@@ -598,8 +598,6 @@ namespace UnityEngine.Rendering.Universal
             // stay in RT so we resume rendering on stack after post-processing
             else if (applyPostProcessing)
             {
-                bool destinationIsInternalRT = afterPostProcessColor == RenderTargetHandle.CameraTarget ||
-                                               afterPostProcessColor.HasInternalRenderTargetId();
                 postProcessPass.Setup(cameraTargetDescriptor, m_ActiveCameraColorAttachment, afterPostProcessColor.id, m_ActiveCameraDepthAttachment, RTHandles.Alloc(colorGradingLut.Identifier()), false, false);
                 EnqueuePass(postProcessPass);
             }
